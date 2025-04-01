3fb0d8b21db454fa5b0280ed7f1912553f001654
Martin <martin.dinahet@proton.me>
2025-04-01 13:17:20 +0200

    chore: format

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,11 +5,11 @@ import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { MainLayout } from "@/layouts/main-layout";
 import { AuthProvider } from "@/components/auth-provider";
-
-import "./index.css";
 import { Index } from "@/pages/index";
 import { Login } from "@/pages/login";
 import { Register } from "@/pages/register";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
